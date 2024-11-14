7e9fb0665cf62504a75c17de0c5e805a4307847c
gaeun19 <xmasge424@gmail.com>
2024-11-14 10:14:57 +0900

    log collection - if you log in

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+# app_name/apps.py
+from django.apps import AppConfig
+from elasticsearch_dsl import connections
 
 
 class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "blog"
+
+
+class AppNameConfig(AppConfig):
+    name = 'blog'  # 실제 앱 이름으로 변경
+
+    def ready(self):
+        # 'default' 연결을 생성하여 Elasticsearch에 연결
+        connections.create_connection(alias='default', hosts=['http://localhost:9200'])
---
+++ blog/logging.py
@@ -0,0 +1,22 @@
+# app_name/logging.py
+from datetime import datetime
+from .documents import UserFlowLogDocument  # documents.py에서 UserFlowLogDocument 가져오기
+
+def log_user_action(user_id, session_id, action, page):
+    """
+    사용자의 행동을 Elasticsearch에 로그로 저장하는 함수.
+
+    Args:
+        user_id (str): 사용자 ID 또는 익명 사용자 구분용 ID.
+        session_id (str): 세션 ID.
+        action (str): 사용자 행동 (예: 페이지 조회, 버튼 클릭).
+        page (str): 페이지 이름 또는 URL.
+    """
+    log = UserFlowLogDocument(
+        user_id=user_id,
+        session_id=session_id,
+        action=action,
+        page=page,
+        timestamp=datetime.utcnow()
+    )
+    log.save()  # Elasticsearch에 로그 저장
