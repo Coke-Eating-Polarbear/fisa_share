dee5f964c0e0c462b35fde4d7f17913268569468
kimnagyeong <knk990928@gmail.com>
2024-11-19 17:27:39 +0900

    maintwo_완료, savings_info1_header/footer_change

--- blog/views.py
+++ blog/views.py
@@ -126,7 +126,7 @@ def main(request):
 
 
 @login_required_session
-def report(request):
+def report_ex(request):
     customer_id = request.session.get('user_id')  
     user_name = "사용자"  # 기본값 설정
 
@@ -141,7 +141,7 @@ def report(request):
     context = {
         'user_name': user_name,
     }
-    return render(request, 'report.html', context)
+    return render(request, 'report_ex.html', context)
 
 def agree(request):
     return render(request, 'join_agree.html')
@@ -193,13 +193,27 @@ def summary_view(request):
     today = timezone.now().date()
     yesterday = today - timedelta(days=1)
     # 오늘 날짜의 이미지 데이터 가져오기
-    try:
-        wc_entry = Wc.objects.filter(date=yesterday).first()
-        image_base64 = base64.b64encode(wc_entry.image).decode('utf-8')  # base64 인코딩
-    except Wc.DoesNotExist:
-        image_base64 = None  # 이미지가 없을 경우 처리
+    # 어제 날짜의 이미지 데이터 가져오기
+    wc_entry = Wc.objects.filter(date=yesterday).first()
+    image_base64 = base64.b64encode(wc_entry.image).decode('utf-8') if wc_entry else None
 
-    news_entries = News.objects.filter(ndate=yesterday, summary__isnull=False).values('title', 'summary')
+    # 어제 날짜의 뉴스 데이터 가져오기
+    news_entries_queryset = News.objects.filter(
+        ndate=yesterday, 
+        summary__isnull=False
+    )
+
+    # 중복 제거 로직
+    seen_titles = set()  # 이미 본 제목을 저장
+    news_entries = []
+    for news in news_entries_queryset:
+        if news.title not in seen_titles:
+            seen_titles.add(news.title)
+            news_entries.append({
+                'title': news.title,
+                'summary': news.summary,
+                'url': news.url  # URL 추가
+            })
 
     # CustomerID가 세션에 없으면 로그인 페이지로 리디렉션
     if not customer_id:
